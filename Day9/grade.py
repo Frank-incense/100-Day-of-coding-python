ca9b43b9e4a353c9916e2d2a81526adbab9a1586
Frankincense Okwemba <67192802+Frank-incense@users.noreply.github.com>
2024-01-19 06:25:34 +0000

    Finally starting Day 11

--- Day9/grade.py
+++ Day9/grade.py
@@ -11,3 +11,15 @@ student_grades = {
 }
 
 # Todo-2
+for scores in student_scores:
+    if student_scores[scores] > 90:
+        student_grades[scores] = "Outstanding"
+    elif student_scores[scores] > 80:
+        student_grades[scores] = "Exceeds Expectation"
+    elif student_scores[scores] > 70:
+        student_grades[scores] = "Acceptable"
+    else:
+        student_grades[scores] = "Fair"
+
+
+print(student_grades)
