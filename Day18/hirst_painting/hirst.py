ad4334536f3dd81ea7c1a236894d40c2ef673c87
Frank Okwemba <fokwemba@kfcb.co.ke>
2024-03-06 15:28:16 +0300

    finally some progress

--- Day18/hirst_painting/hirst.py
+++ Day18/hirst_painting/hirst.py
@@ -1,4 +1,18 @@
 import colorgram
+import turtle as t
 
-colors = colorgram.extract('spots.jpg', 30)
-print(colors)
+frank = t.Turtle
+screen = t.TurtleScreen
+
+new_color = []
+colors = colorgram.extract("C:/Users/fokwemba/Documents/code/100-Day-of-coding-python/Day18/hirst_painting/spots.jpg", 30)
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color.append((r,g,b))
+
+print(new_color)
+
+
+t.exitonclick()
