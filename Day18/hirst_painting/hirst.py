f5e1aa9b4217ef0fa757bdc8b57858a51a9e7440
frankincense <frankincensewesley@gmail.com>
2024-04-15 14:05:30 +0300

    turtle done

--- Day18/hirst_painting/hirst.py
+++ Day18/hirst_painting/hirst.py
@@ -1,18 +1,28 @@
-import colorgram
-import turtle as t
+import random as random
+import turtle as turtle_module
 
-frank = t.Turtle
-screen = t.TurtleScreen
+frank = turtle_module.Turtle()
+screen = turtle_module.Screen()
+screen.colormode(255)
 
-new_color = []
-colors = colorgram.extract("C:/Users/fokwemba/Documents/code/100-Day-of-coding-python/Day18/hirst_painting/spots.jpg", 30)
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color.append((r,g,b))
+color = [(249, 228, 18), (212, 13, 9), (43, 212, 70), (234, 149, 40), (33, 31, 152), (16, 22, 55), (66, 9, 48),  (244, 39, 149), (65, 203, 229), (223, 20, 110), (229, 164, 9), (15, 154, 23), (245, 57, 16), (98, 75, 9), (223, 139, 203), (67, 241, 160), (10, 97, 61)]
 
-print(new_color)
+frank.penup()
+frank.setheading(225)
+frank.forward(250)
+frank.setheading(0)
 
 
-t.exitonclick()
+for i in range(10):
+    for _ in range(10):
+        frank.setheading(0)
+        frank.dot(20, random.choice(color))
+        frank.forward(50)
+        
+    frank.left(90)
+    frank.forward(50)
+    frank.left(90)
+    frank.forward(500)
+ 
+
+screen.exitonclick()
