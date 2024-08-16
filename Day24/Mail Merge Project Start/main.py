dcd014efd3c7c369b5d8f139667a60e9a63fda5a
frankincense <frankincensewesley@gmail.com>
2024-08-16 10:30:55 +0300

    easy this time

--- Day24/Mail Merge Project Start/main.py
+++ Day24/Mail Merge Project Start/main.py
@@ -1,8 +1,20 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# Open the file and store the letter as variables.
+with open("./100-Day-of-coding-python/Day24/Mail Merge Project Start/Input/Letters/starting_letter.txt") as startLetter:
+    toEdit = startLetter.read()
+
+# Open the file and store the letter as variables.
+with open("./100-Day-of-coding-python/Day24/Mail Merge Project Start/Input/Names/invited_names.txt") as names:
+    toAdd = names.read()
+
+# Make the names into a list
+nameList = toAdd.split()
+
+# Iterate through the list of names
+for name in nameList:
+
+    # Replace the [name] with a name in the list
+    newLetter = toEdit.replace("[name]", name)
+
+    # Write a new letter and save it
+    with open(f"./100-Day-of-coding-python/Day24/Mail Merge Project Start/Output/ReadyToSend/{name}.txt", mode="w") as editedLetter:
+        editedLetter.write(newLetter)
